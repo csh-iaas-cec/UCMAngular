a54bfc1f3a1ed91c40eb7cc23717e984e30cfe86
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2019-12-11 02:14:57 +0530

    Merge conflict

--- src/app/_components/reports-billing/best-practices/best-practices.component.ts
+++ src/app/_components/reports-billing/best-practices/best-practices.component.ts
@@ -3,7 +3,7 @@ import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree'
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { CloudPortalService } from '../../../_services/cloud-portal.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-<<<<<<< HEAD
+
 import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 
@@ -30,38 +30,14 @@ export interface InterfaceCompute {
 }
 
 
-=======
 
-interface Tenancy1Node {
-  name2: string;
-  name3: string;
-}
-interface TenancyNode {
-  name1: string;
-  children: Tenancy1Node[];
-}
-interface FoodNode {
-  name: string;
-  children: FoodNode[];
-
-}
-
-
-
-/** Flat node with expandable and level information */
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
->>>>>>> 2891e135bbcb28a3c31067887b4aef66f2310e4a
 @Component({
   selector: 'app-best-practices',
   templateUrl: './best-practices.component.html',
   styleUrls: ['./best-practices.component.scss']
 })
 export class BestPracticesComponent implements AfterViewInit {
-<<<<<<< HEAD
+
   
   dataSource: any ;
 
@@ -70,32 +46,6 @@ export class BestPracticesComponent implements AfterViewInit {
   matTressComputeActive: Boolean = false;
 
   displayedColumns: string[] = [];
-=======
-  private transformer = (node: FoodNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  }
-
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level, node => node.expandable);
-
-  treeFlattener = new MatTreeFlattener(
-    this.transformer, node => node.level, node => node.expandable, node => node.children);
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-  dataSource1 = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-  dataSourceiam = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-  TREE_DATA: FoodNode[] = [];
-  TREE_DATA_1: FoodNode[] = [];
-  TREE_DATA_2: FoodNode[] = [];
-
-  matTressNetworkisActive: Boolean = false;
-  matTressIAMActive: Boolean = false;
->>>>>>> 2891e135bbcb28a3c31067887b4aef66f2310e4a
 
   tenancyname: any;
   reports: any;
@@ -103,15 +53,12 @@ export class BestPracticesComponent implements AfterViewInit {
   selected_tenancy: any; 
 
   secuityList: any;
-<<<<<<< HEAD
 
   iam_data: InterfaceIAM[] = [];
   network_data: InterfaceNetwork[] = [];
   compute_data: InterfaceCompute[] = [];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-=======
->>>>>>> 2891e135bbcb28a3c31067887b4aef66f2310e4a
   constructor(private cloudservice: CloudPortalService, private spinner: NgxSpinnerService) {
 
   }
@@ -134,12 +81,6 @@ export class BestPracticesComponent implements AfterViewInit {
 
   }
 
-<<<<<<< HEAD
-=======
-  ngOnInit() {
-
-  }
->>>>>>> 2891e135bbcb28a3c31067887b4aef66f2310e4a
 
   selectchangetenancy(selected_tenancy){
     console.log('select change called')
@@ -154,13 +95,8 @@ export class BestPracticesComponent implements AfterViewInit {
         this.secuityList = secList;
         this.spinner.hide();
       }, err => {
-<<<<<<< HEAD
         console.log('errrr', err);
       });
-=======
-        console.log('errrr', err)
-      })
->>>>>>> 2891e135bbcb28a3c31067887b4aef66f2310e4a
 
   }
 
@@ -168,7 +104,6 @@ export class BestPracticesComponent implements AfterViewInit {
 
     this.matTressNetworkisActive = true;
     this.matTressIAMActive = false;
-<<<<<<< HEAD
     this.matTressComputeActive = false;
     this.displayedColumns = [
       'compartment_name',
@@ -229,68 +164,3 @@ export class BestPracticesComponent implements AfterViewInit {
     this.matTressIAMActive = false;
   }
 }
-=======
-    const matTree = document.querySelector('treeColorRed');
-    this.TREE_DATA = [
-      {
-        name: 'Security list exposed to Public Internet',
-        children: secList.securityinternet
-      },
-      {
-        name: 'Security list allowing All Protocols',
-        children: secList.securityallprotocol
-      },
-      {
-        name: 'Security list having all ports open',
-        children: secList.securityallports
-      }
-
-    ];
-
-    this.TREE_DATA_1 = [{
-      name: 'Security list having port 80 exposed to Public internet',
-      children: secList.securityport80
-    },
-    {
-      name: 'Security list having port 7001 exposed to Public internet',
-      children: secList.securityport7001
-    }]
-    console.log(this.TREE_DATA);
-    this.dataSource.data = this.TREE_DATA;
-    this.dataSource1.data = this.TREE_DATA_1;
-  }
-
-  openViewReportIAM(secList) {
-    console.log(secList)
-    this.matTressNetworkisActive = false;
-    this.matTressIAMActive = true;
-    const matTree = document.querySelector('treeColorRed');
-    secList = secList[0]
-    this.TREE_DATA_2 = [
-      {
-        name: ' Users who are invalid',
-        children: secList.inValid
-      },
-      {
-        name: 'Local Users',
-        children: secList.local
-      },
-      {
-        name: ' Local users without MFA enabled',
-        children: secList.localWithoutMFA
-      },
-      {
-        name: 'Service Users',
-        children: secList.service
-      }
-
-    ];
-
-
-    console.log(this.TREE_DATA_2);
-    this.dataSourceiam.data = this.TREE_DATA_2;
-  }
-
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-}
->>>>>>> 2891e135bbcb28a3c31067887b4aef66f2310e4a
